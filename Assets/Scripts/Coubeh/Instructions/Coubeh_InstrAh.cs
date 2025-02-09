e86c528d6cbadd6f52399d5145dba5f2053f6420
Selenn Lamson <contact@selenn-lamson.com>
2025-02-09 21:07:11 +0100

    Added comment instruction

---
+++ Assets/Scripts/Coubeh/Instructions/Coubeh_InstrAh.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coubeh_InstrAh : CoubehInstruction
+{
+    public override void Run(CoubehRunner.CoubehRunnerInstance coubehRunnerInstance, List<string> parameters)
+    {
+        // Nothing, head empty, move on
+    }
+}
